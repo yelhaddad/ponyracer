b130d6ebae08c79bac72f71d5b9f0be2ef8cecc9
Yassine El Haddad <yassineelhaddad@MacBook-Pro-de-Yassine.local>
2017-02-22 11:31:18 +0100

    Excercice 14 done

--- ponyracer/src/app/menu/menu.component.ts
+++ ponyracer/src/app/menu/menu.component.ts
@@ -1,17 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+
+import { UserModel } from '../models/user.model';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'pr-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent implements OnInit, OnDestroy {
 
   navbarCollapsed = true;
 
-  constructor() {}
+  user: UserModel;
+  userEventsSubscription: Subscription;
+
+  constructor(private userService: UserService) {
+  }
 
   ngOnInit() {
+    this.userEventsSubscription = this.userService.userEvents.subscribe(user => this.user = user);
+  }
+
+  ngOnDestroy() {
+    if (this.userEventsSubscription) {
+      this.userEventsSubscription.unsubscribe();
+    }
   }
 
   toggleNavbar() {
--- ponyracer/src/app/user.service.ts
+++ ponyracer/src/app/user.service.ts
@@ -1,24 +1,31 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+
+import { UserModel } from './models/user.model';
 
 @Injectable()
 export class UserService {
 
+  public userEvents = new Subject<UserModel>();
+
   constructor(private http: Http) {
   }
 
-  register(login, password, birthYear): Observable<any> {
+  register(login, password, birthYear): Observable<UserModel> {
     const body = {login, password, birthYear};
     return this.http.post('http://ponyracer.ninja-squad.com/api/users', body)
       .map(res => res.json());
   }
 
-  authenticate(credentials): Observable<any> {
+  authenticate(credentials): Observable<UserModel> {
     return this.http
       .post('http://ponyracer.ninja-squad.com/api/users/authentication', credentials)
-      .map(res => res.json());
+      .map(res => res.json())
+      .do((user: UserModel) => this.userEvents.next(user));
   }
 
 }
